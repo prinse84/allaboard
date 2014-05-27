40aa2dcc54bae4b0572b91e15f10bf50cda3533c
Ray King <raymond.a.king@gmail.com>
2014-05-27 18:50:25 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Allaboard::Application.config.secret_key_base = '193c5e275020bf94c7c01b3775ce6828f4fa707f70d39398c45977bbae416fce2f10efa9712714a55147e194ee4fa84d2cb52c053eaa55ade09c2057281635f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Allaboard::Application.config.session_store :cookie_store, key: '_allaboard_session'
