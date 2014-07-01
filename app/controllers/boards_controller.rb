bff0a6f2c1553ba2befa18d7dad60dd1c0ed504c
Ray King <raymond.a.king@gmail.com>
2014-07-01 16:43:26 -0500

    beta fixes

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,7 +3,7 @@ class BoardsController < ApplicationController
   before_action :authenticate_user!, :only => [:claim]
   
   def index
-    @boards = Board.all.paginate(:page => params[:page], :per_page => 10).order('created_at DESC')
+    @boards = Board.all.paginate(:page => params[:page], :per_page => 10).order('name')
   end
   
   def show
