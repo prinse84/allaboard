606d2cca7f40e3e77e37584a26de8d9dcc9f3a4b
Gopi Vajravelu <GopiVajravelu2012@u.northwestern.edu>
2014-07-20 15:33:37 -0500

    admin-dashboard: add admin field in user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717001601) do
+ActiveRecord::Schema.define(version: 20140720201502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20140717001601) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
