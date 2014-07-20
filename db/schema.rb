231b3999d3a6b33d74c93f7ff1b5712668b6191f
Ray King <raymond.a.king@gmail.com>
2014-07-20 13:16:08 -0500

    new merge

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140717001601) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "boards", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -81,8 +78,8 @@ ActiveRecord::Schema.define(version: 20140717001601) do
     t.string   "last_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "vendor_reviews", force: true do |t|
     t.string   "title"
