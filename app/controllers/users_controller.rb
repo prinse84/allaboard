0b45217730b22b74d5cf953242dc68ab87bce88f
Ray King <raymond.a.king@gmail.com>
2014-07-26 12:03:44 -0500

    Working on user account management.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   
   def show
      if params[:id].to_i == current_user.id
         @user = User.find(params[:id])
+        @boards = @user.boards.paginate(:page => params[:page], :per_page => 5).order('name')
       else
        raise("not found")
      end
--- db/schema.rb
+++ db/schema.rb
@@ -62,12 +62,12 @@ ActiveRecord::Schema.define(version: 20140720201502) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20140720201502) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
