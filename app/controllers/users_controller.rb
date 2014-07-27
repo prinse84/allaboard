4eb9d8f0f63a1bafe1beea4989b07a2ddd7edf02
Gopi Vajravelu <GopiVajravelu2012@u.northwestern.edu>
2014-07-26 19:43:31 -0500

    admin-dashboard: admins can see user show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
   
   def show
-     if params[:id].to_i == current_user.id
+     if params[:id].to_i == current_user.id || current_user.admin
         @user = User.find(params[:id])
         @boards = @user.boards.paginate(:page => params[:page], :per_page => 5).order('name')
       else
