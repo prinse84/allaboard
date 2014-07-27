97bf172b7c4bc447b7dcbe100cd79c4a7140572f
Gopi Vajravelu <GopiVajravelu2012@u.northwestern.edu>
2014-07-26 20:01:38 -0500

    admin-dashboard: admin can delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_user_before_destroy, :only => [:destroy]
 
   def index
     @users = User.all.paginate(:page => params[:page], :per_page => 20).order('first_name, last_name')
@@ -12,4 +14,23 @@ class UsersController < ApplicationController
        raise("not found")
      end
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    for board in @user.boards.all
+      board.update_attribute(:user_id, nil)
+    end
+    @user.destroy
+    flash[:success] = "The user has been deleted."
+    redirect_to users_path
+  end
+
+  private
+
+    def check_user_before_destroy
+      if current_user && params[:id] != current_user.id && current_user.admin
+        return true
+      end
+      return false
+    end
 end
