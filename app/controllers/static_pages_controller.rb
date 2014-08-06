593a52001ed9a742d94875584afdd2351e5783d4
Ray King <raymond.a.king@gmail.com>
2014-08-05 20:10:18 -0500

    Updating static page contents.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @reviews = Review.all
-    ids = Review.pluck(:board_id).shuffle[0..2]
+    ids = Review.pluck(:board_id).shuffle[0..1]
     @boards = Board.where(id: ids)
     @events = Event.where("date >= ?", Time.now).order('date').limit(7)
   end
